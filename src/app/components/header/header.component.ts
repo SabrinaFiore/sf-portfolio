ca4db8d71cd0974ce0b8353bd1c37014a87c8e1e
Sabrina Fiore <sabrinafiore1992@gmail.com>
2022-04-23 15:30:23 +0200

    get home ad work-experience page responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,8 +9,8 @@ export class HeaderComponent implements OnInit {
 
   links = [
     // { id: 1, name: 'Landing', path: './view-landing' },
-    { id: 2, name: 'Work', path: './view-work' },
-    { id: 3, name: 'Work 2', path: './view-work-two' },
+    // { id: 2, name: 'Work', path: './view-work' },
+    { id: 3, name: 'Work Expericence', path: './view-work-two' },
     { id: 3, name: 'Personal Projects', path: './view-gallery' },
   ];
 
