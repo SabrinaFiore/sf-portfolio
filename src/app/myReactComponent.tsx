6dffb5bb49e1695b9a8134f1df30b27cfa19581e
Sabrina Fiore <sabrinafiore1992@gmail.com>
2022-04-25 10:00:14 +0200

    add webpack

--- src/app/myReactComponent.tsx
+++ src/app/myReactComponent.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
+// import glsl from 'babel-plugin-glsl/macro';
+// import { shaderMaterial } from "@react-three/drei";
+
 
 const myReactComponent = () => {
+
+
   return(
     <div>Test</div>
   )
